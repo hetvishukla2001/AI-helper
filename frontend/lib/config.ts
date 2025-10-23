52d282643c9a6aa86bd44cad0c401c482b9f7f8e
hetvishukla2001 <hetvishukla2001@gmail.com>
2025-10-23 02:07:06 -0400

    fix:add new working url

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,2 +1,4 @@
+// temp change to trigger rebuild
+console.log("Vercel rebuild test"+process.env.NEXT_PUBLIC_API_BASE_URL);
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001';
